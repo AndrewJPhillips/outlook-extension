00f79b2f42ccfaf9ea6c792aea2e797be906f19e
Andy J Phillips <andrew.j.phillips@hotmail.co.uk>
2020-03-01 16:58:59 +0000

    Add production build.

---
+++ build/precache-manifest.621b319d80e452c3b7679bbfd66651f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8ae26aa5a5c1f6ef6bf08f267da0eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a8a9baf2a0804ed992a",
+    "url": "/static/css/main.9c7ce108.chunk.css"
+  },
+  {
+    "revision": "51bf3c317fb15f40e323",
+    "url": "/static/js/2.d5e346d3.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.d5e346d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a8a9baf2a0804ed992a",
+    "url": "/static/js/main.81ed7c00.chunk.js"
+  },
+  {
+    "revision": "4da93d8e0d58b7a3b517",
+    "url": "/static/js/runtime-main.1be120ca.js"
+  }
+]);
